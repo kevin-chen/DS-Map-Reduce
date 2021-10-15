8025fb30d73d152ab4b7bc5e95cccd6ae322e113
Kevin Chen <kevin-chen@users.noreply.github.com>
2021-10-15 13:57:49 -0400

    initial

---
+++ src/mapreduce/master.go
@@ -0,0 +1,30 @@
+package mapreduce
+
+import "fmt"
+
+type WorkerInfo struct {
+	address string
+}
+
+// Clean up all workers by sending a Shutdown RPC to each one of them Collect
+// the number of jobs each work has performed.
+func (mr *MapReduce) KillWorkers() []int {
+	l := make([]int,0)
+	for _, w := range mr.Workers {
+		DPrintf("DoWork: shutdown %s\n", w.address)
+		args := &ShutdownArgs{}
+		var reply ShutdownReply
+		ok := call(w.address, "Worker.Shutdown", args, &reply)
+		if ok == false || reply.OK == false {
+			fmt.Printf("DoWork: RPC %s shutdown error\n", w.address)
+		} else {
+			l=append(l,reply.Njobs)
+		}
+	}
+	return l
+}
+
+func (mr *MapReduce) RunMaster() []int {
+	// TODO your code here
+    return mr.KillWorkers()
+}
