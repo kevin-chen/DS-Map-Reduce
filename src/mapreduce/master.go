3a9d4eb84cfd75e1171ccdbd8c7151a96ab0ea42
Kevin Chen <kevin-chen@users.noreply.github.com>
2021-10-18 18:24:23 -0400

    test registerChannel for parallel map/reduce jobs

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -9,7 +9,7 @@ type WorkerInfo struct {
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
 // the number of jobs each work has performed.
 func (mr *MapReduce) KillWorkers() []int {
-	l := make([]int,0)
+	l := make([]int, 0)
 	for _, w := range mr.Workers {
 		DPrintf("DoWork: shutdown %s\n", w.address)
 		args := &ShutdownArgs{}
@@ -18,13 +18,23 @@ func (mr *MapReduce) KillWorkers() []int {
 		if ok == false || reply.OK == false {
 			fmt.Printf("DoWork: RPC %s shutdown error\n", w.address)
 		} else {
-			l=append(l,reply.Njobs)
+			l = append(l, reply.Njobs)
 		}
 	}
 	return l
 }
 
 func (mr *MapReduce) RunMaster() []int {
-	// TODO your code here
-    return mr.KillWorkers()
+	numMapJobs := mr.nMap
+	numReduceJobs := mr.nReduce
+
+	for mapJob := 0; mapJob < numMapJobs; mapJob++ {
+		fmt.Println(<-mr.registerChannel)
+	}
+
+	for reduceJob := 0; reduceJob < numReduceJobs; reduceJob++ {
+
+	}
+
+	return mr.KillWorkers()
 }
